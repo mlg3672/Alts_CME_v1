49dfc2f4d5c4e8f2754b99b24215e734098ff343
Michele Goe <michele.goe@gmail.com>
2025-02-25 08:01:58 -0500

    added chart

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,34 @@
 import streamlit as st
+import numpy as np
+import time
+import pandas as pd
+
 
 st.title("🎈 My new app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's start building hey hey ehy ! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+progress_bar = st.sidebar.progress(0)
+status_text = st.sidebar.empty()
+last_rows = np.random.randn(1, 1)
+chart = st.line_chart(last_rows)
+
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+st.line_chart(chart_data)
+
+
+for i in range(1, 101):
+    new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
+    status_text.text(f"{i}% complete")
+    chart.add_rows(new_rows)
+    progress_bar.progress(i)
+    last_rows = new_rows
+    time.sleep(0.05)
+
+progress_bar.empty()
+
+# Streamlit widgets automatically run the script from top to bottom. Since
+# this button is not connected to any other logic, it just causes a plain
+# rerun.
+st.button("Rerun")
